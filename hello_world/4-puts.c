1332198b58e36033db98db8bbc8cded4db309b58
JunHabbies <9553@holbertonstudents.com>
2024-06-17 13:21:20 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a message using the puts function.
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
