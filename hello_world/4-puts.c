56d2323be766cdeb2ccbcbecfb0d1b42671e47f1
JunHabbies <9553@holbertonstudents.com>
2024-06-17 13:17:28 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int main (void)
-	{
-		puts("\"Programming is like building a multilingual puzzle");
-		
-		return(0);
-	}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 
+	return (0);
+}
