e73a26f91627589876650120b8beadfe29e95421
JunHabbies <9553@holbertonstudents.com>
2024-06-19 09:05:53 +0000

    test

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * ft_print - Affiche les chiffres de '0' à '9'
+ *
+ * Description:
+ * Cette fonction affiche les chiffres
+ * en utilisant la fonction printf pour chaque caractère.
+ */
+
+
+void ft_print (void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		printf("%c", a);
+		a++;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Fonction principale
+ *
+ * Description:
+ * Fonction principale du programme qui appelle ft_print pour afficher
+ * les lettres de l'alphabet et retourne 0
+ *
+ * Return: 0 si le programme s'exécute correctement
+ */
+
+int main(void)
+{
+	ft_print();
+	return (0);
+}
+
