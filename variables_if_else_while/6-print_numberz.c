1e96660565ceae6a429e3db95fad61f521c0b7e3
JunHabbies <9553@holbertonstudents.com>
2024-06-19 09:40:16 +0000

    test

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * ft_print - Affiche les chiffres de '0' à '9'
+ *
+ * Description:
+ * Cette fonction affiche les chiffres
+ * en utilisant la fonction write pour chaque caractère.
+ */
 
 void ft_print (void)
 {
@@ -11,8 +18,18 @@ void ft_print (void)
 		write(1, &a, 1);
 		a++;
 	}
-	write(1,"\n",1);
+	write(1, "\n", 1);
 }
+
+/**
+ * main - Fonction principale
+ *
+ * Description:
+ * Fonction principale du programme qui appelle ft_print pour afficher
+ * les lettres de l'alphabet et retourne 0
+ *
+ * Return: 0 si le programme s'exécute correctement
+ */
 
 int main(void)
 {
