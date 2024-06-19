cd62237067e7ea4eea2b41f609f089a5b9180d34
JunHabbies <9553@holbertonstudents.com>
2024-06-19 09:43:19 +0000

    test

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * ft_print - Affiche les lettres de 'z' à 'a'
+ *
+ * Description:
+ * Cette fonction affiche les lettres de l'alphabet en minuscule
+ * en utilisant la fonction printf pour chaque caractère.
+ */
+
+void ft_print (void)
+{
+	int a = 'z';
+
+	while (a >= 'a')
+	{
+		printf("%c", a);
+		a--;
+	}
+	printf("\n");
+}
+
+/**
+ * main - Fonction principale
+ *
+ * Description:
+ * Fonction principale du programme qui appelle ft_print pour afficher
+ * les lettres de l'alphabet et retourne 0
+ *
+ * Return: 0 si le programme s'exécute correctement
+ */
+
+int main(void)
+{
+	ft_print();
+	return (0);
+}
