8a6e2167bd82cc7486ccb931f7f7407bf576455a
JunHabbies <9553@holbertonstudents.com>
2024-06-19 08:27:31 +0000

    test

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * ft_print - Affiche les lettres de 'a' à 'z'
+ *
+ * Description:
+ * Cette fonction affiche les lettres de l'alphabet en minuscule
+ * en utilisant la fonction printf pour chaque caractère.
+ */
+
 void ft_print (void)
 {
 	int a = 'a';
@@ -10,6 +18,15 @@ void ft_print (void)
 		a++;
 	}
 }
+
+/**
+ * ft_print_upper - Affiche les lettres de 'A' à 'Z' suivies d'un saut de ligne
+ *
+ * Description:
+ * Cette fonction affiche les lettres de l'alphabet en majuscule
+ * en utilisant la fonction printf pour chaque caractère, puis ajoute
+ * un saut de ligne à la fin de l'affichage.
+ */
 
 void ft_print_upper(void)
 {
@@ -22,6 +39,18 @@ void ft_print_upper(void)
 	}
 	printf("\n");
 }
+
+/**
+ * main - Fonction principale du programme
+ *
+ * Description:
+ * Fonction principale qui appelle ft_print pour afficher les lettres
+ * de l'alphabet en minuscule, puis ft_print_upper pour afficher les lettres
+ * de l'alphabet en majuscule suivies d'un saut de ligne. Retourne 0 pour
+ * indiquer une exécution normale du programme.
+ *
+ * Return: 0 si le programme s'est terminé correctement
+ */
 
 int main(void)
 {
