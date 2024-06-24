354a871531b5a2759d150c27f8b0fa95bf1053ae
JunHabbies <9553@holbertonstudents.com>
2024-06-24 09:11:48 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,17 @@
 void more_numbers(void)
 {
 	int a;
-	char b;
+	int b;
 
 	for (a = 1; a <= 10; a++)
 	{
-		for (b = '0'; b <= '14'; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			if (b >= 10)
+			{
+				_putchar(b / 10+'0');
+			}
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
