db2d342a6ffc1c7397276011c871605749a19cd7
JunHabbies <9553@holbertonstudents.com>
2024-06-28 08:49:22 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,33 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string, or 0 if no numbers found
- */
+* _atoi - Convert string to an integer.
+* @s: Pointer to a character string.
+*
+* Return: void.
+*/
 
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int found_num = 0;
+	int sign;
+	unsigned int num;
+	char *temp;
 
-    // Traverse through the string
-    while (*s)
-    {
-        // Check for sign and update it
-        if (*s == '-')
-        {
-            sign *= -1;
-        }
-        else if (*s == '+')
-        {
-            // Continue if a plus sign is found (does not change the sign)
-        }
-        // If the current character is a digit
-        else if (*s >= '0' && *s <= '9')
-        {
-            found_num = 1;
-            result = result * 10 + (*s - '0');
-        }
-        // If a number has been found and a non-digit is encountered, break
-        else if (found_num)
-        {
-            break;
-        }
-        s++;
-    }
-
-    return sign * result;
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
-
